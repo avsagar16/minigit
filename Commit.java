784eb4ce6149ff041e8e2cd874e33efb43ddc187
Adithya Sagar <117886463+avsagar16@users.noreply.github.com>
2025-03-24 21:59:48 -0400

    Add files via upload

---
+++ Commit.java
@@ -0,0 +1,95 @@
+<<<<<<< HEAD
+package gitlet;
+
+import java.io.Serializable;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.HashMap;
+import java.text.SimpleDateFormat;
+
+
+/** Represents a gitlet commit object.
+ *  This is the implementation for a Gitlet commit object. It stores information like the commit
+ *  message, UID, time, and the same data for the parent UID.
+ *
+ *  @author Adithya Sagar
+ */
+public class Commit implements Serializable {
+    /**
+     *
+     * List all instance variables of the Commit class here with a useful
+     * comment above them describing what that variable represents and how that
+     * variable is used. We've provided one example for `message`.
+     */
+
+    /** The message of this Commit. */
+    private String message;
+    /** A HashMap that stores the name and UID. */
+    private HashMap<String, String> tracker;
+    /** The UID (SHA1) of a commit. */
+    private String UID;
+    /** The UID (SHA1) of the parent commit. */
+    private String parentUID;
+    /** The timestamp of this Commit. */
+    private String time;
+    private String parent2;
+
+    public Commit(String message, HashMap<String, String> tracker, String cParent) {
+        this.message = message;
+        this.tracker = tracker;
+        this.parentUID = cParent;
+        this.parent2 = null;
+
+        if (this.parentUID == null) {
+            SimpleDateFormat formatter = new SimpleDateFormat("EEE MMM d HH:mm:ss yyyy Z");
+            this.time = formatter.format(new Date(0));
+        } else {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEE MMM d HH:mm:ss yyyy Z");
+            this.time = formatter.format(ZonedDateTime.now());
+        }
+        UID = makeUID();
+
+    }
+    public Commit(String message, HashMap<String, String> tracker, String cParent,
+                  String mergeParent) {
+        this.message = message;
+        this.tracker = tracker;
+        this.parentUID = cParent;
+        this.parent2 = mergeParent;
+
+        if (this.parentUID == null) {
+            SimpleDateFormat formatter = new SimpleDateFormat("EEE MMM d HH:mm:ss yyyy Z");
+            this.time = formatter.format(new Date(0));
+        } else {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEE MMM d HH:mm:ss yyyy Z");
+            this.time = formatter.format(ZonedDateTime.now());
+        }
+        UID = makeUID();
+
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+    public String makeUID() {
+        byte[] commitBlob = Utils.serialize(this);
+        return Utils.sha1(commitBlob);
+    }
+
+    public String getTime() {
+        return this.time;
+    }
+    public String getUID() {
+        return this.UID;
+    }
+    public String getParentUID() {
+        return this.parentUID;
+    }
+    public HashMap<String, String> getBlobs() {
+        return this.tracker;
+    }
+    public String getMergeParent() {
+        return this.parent2;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,70 @@
+<<<<<<< HEAD
+package gitlet;
+
+/** Driver class for Gitlet, a subset of the Git version-control system.
+ *  @author Adithya Sagar
+ */
+public class Main {
+
+    /** Usage: java gitlet.Main ARGS, where ARGS contains
+     *  <COMMAND> <OPERAND1> <OPERAND2> ... 
+     */
+    public static void main(String[] args) {
+        Repository repo = new Repository();
+        if (args.length == 0) {
+            exitWithError("Please enter a command.");
+        }
+        String firstArg = args[0];
+        switch (firstArg) {
+            case "init":
+                repo.init();
+                break;
+            case "add":
+                repo.add(args[1]);
+                break;
+            case "commit":
+                repo.commit(args[1]);
+                break;
+            case "checkout":
+                repo.checkout(args);
+                break;
+            case "log":
+                repo.log();
+                break;
+            case "rm":
+                repo.rm(args[1]);
+                break;
+            case "find":
+                repo.find(args[1]);
+                break;
+            case "branch":
+                repo.branch(args[1]);
+                break;
+            case "global-log":
+                repo.globalLog();
+                break;
+            case "status":
+                repo.status();
+                break;
+            case "reset":
+                repo.reset(args[1]);
+                break;
+            case "rm-branch":
+                repo.rmBranch(args[1]);
+                break;
+            case "merge":
+                repo.merge(args[1]);
+                break;
+            default:
+                Main.exitWithError("No command with that name exists.");
+
+        }
+    }
+
+    public static void exitWithError(String message) {
+        if (message != null && !message.equals("")) {
+            System.out.println(message);
+        }
+        System.exit(0);
+    }
+}
